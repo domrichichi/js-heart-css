7dfa8b7063e28191892040a67ec1d797cc983f43
domrichichi <domrichichi@gmail.com>
2016-03-08 15:48:39 -0500

    Selected for first child

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@
 
 var $header = $('header');
 var $h1 = $('h1');
-var $main = $('main')
-var $p = $('main p:first-child')
-var $h2 = $('h2')
-var $ul = $('main ul')
+var $main = $('main');
+var $p = $('main p:first-child');
+var $h2 = $('h2');
+var $ul = $('main ul');
+var $img = $('img');
+var $first = $('main ul:first-child')
+var $last
 
 
 $header.addClass('masthead');
 $h1.removeClass('big-heading');
 $h1.addClass('logo');
 $main.addClass('wrapper');
-$p.addClass('intro')
-$h2.addClass('slug-head')
-
+$p.addClass('intro');
+$h2.addClass('slug-head');
+$ul.addClass('slug-list');
+$img.addClass('slug-img');
+$first.addClass('slug-list-first')
+$
