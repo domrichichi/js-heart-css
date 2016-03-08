ba38e38a9d92289c61396d47b4ec3f86b8968965
domrichichi <domrichichi@gmail.com>
2016-03-08 15:41:56 -0500

    Revised Selections

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 
 
-var $header = $('header')
-var $masthead.addClass = ('masthead')
-var $big-heading = $('big-heading').remove()
+var $header = $('header');
+var $h1 = $('h1');
 var $main = $('main')
-var $header = $('header')
-var $header = $('header')
+var $p = $('main p:first-child')
+var $h2 = $('h2')
+var $ul = $('main ul')
+
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+$p.addClass('intro')
+$h2.addClass('slug-head')
+
