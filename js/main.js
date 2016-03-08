d5569896ac2f2335a008ce6f8f54f2521bc0a3fd
domrichichi <domrichichi@gmail.com>
2016-03-08 15:21:22 -0500

    Adding Classes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+var $header = $('header')
+var $masthead.addClass = ('masthead')
+var $big-heading = $('big-heading').remove()
+var $main = $('main')
+var $header = $('header')
+var $header = $('header')
